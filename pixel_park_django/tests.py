3396d80cf41c6c448c47f836d09c3af95b30c356
Albert <albert.maharjan94@gmail.com>
2020-02-16 20:10:59 +0545

    testing init

--- pixel_park_django/tests.py
+++ pixel_park_django/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models.Post import Post
+from .models.User import User
+from .models.Comment import Comment
+
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(name="albert", username="test.user", email="test@gmail.com", is_admin=0)
+        user2 = User.objects.create(name="test", username="test2.user", email="test2@gmail.com", is_admin=0)
+        Post.objects.create(caption="This is a test post", user_id=user.id)
+
+    def test_user(self):
+        user = User.objects.get(username="test.user")
+        self.assertEqual(user.email, 'test@gmail.com')
+
+    def test_Post(self):
+        post = Post.objects.get(caption="This is a test post")
+        self.assertEqual(post.user.name, 'albert')
+
+    def test_Comment(self):
+        user2 = User.objects.get(username="test2.user")
+        user = User.objects.get(username="test.user")
+        post = Post.objects.get(caption="This is a test post")
+        comment = Comment.objects.create(post_id=post.id, user_id=user2.id)
+
+        self.assertEqual(comment.user.id, user2.id)
